17941428186a6d63990f6241b978c3ef50cea0f9
jorgigonzaleza <jorgi.gonzaleza@duocuc.cl>
2026-09-11 16:23:40 -0300

    Clases

--- Ejercicio3/src/Libro.java
+++ Ejercicio3/src/Libro.java
@@ -1,2 +1,12 @@
 public class Libro {
+    protected String titulo, autor;
+    protected int anno;
+    protected boolean disponible;
+
+    public Libro(String titulo, String autor, int anno, boolean disponible) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anno = anno;
+        this.disponible = disponible;
+    }
 }
