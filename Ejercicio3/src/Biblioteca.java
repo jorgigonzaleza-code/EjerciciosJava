dc73e79cecbb3f013ac2812155ed9e1780027e6a
jorgigonzaleza <jorgi.gonzaleza@duocuc.cl>
2026-09-11 17:41:32 -0300

    Avance final clase

--- Ejercicio3/src/Biblioteca.java
+++ Ejercicio3/src/Biblioteca.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class Biblioteca
 {
+    private ArrayList<Libro> conjuntoLibros;
+
+    public Biblioteca() {
+        conjuntoLibros = new ArrayList<>();
+    }
+
+    public void agregarLibro(Libro libro){
+        conjuntoLibros.add(libro);
+    }
+
+    public void mostrarDatos(){
+        System.out.println("---BIBLIO---");
+
+        for (Libro libro : conjuntoLibros) {
+            System.out.println(libro);
+
+        }
+    }
+
 }
--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -5,6 +5,7 @@ public class Main
         Fisico libro1 = new Fisico("El principico", "Antoine", 1943, true, 50, "Disponible");
         Digital libro2 = new Digital("Harry potter", "Anne", 1950, true, "Digital", 500);
         Digital libro3 = new Digital("pdf", 21.4);
+        Digital libro4 = new Digital();
 
         libro1.mostrarInformacion();
         System.out.println("------");
@@ -13,6 +14,17 @@ public class Main
         System.out.println("---");
         System.out.println(libro2);
         System.out.println(libro3);
+        System.out.println(libro4);
+
+
+        Biblioteca biblio = new Biblioteca();
+
+        biblio.agregarLibro(libro1);
+        biblio.agregarLibro(libro2);
+        //biblio.agregarLibro(libro3);
+        //biblio.agregarLibro(libro4);
+
+        biblio.mostrarDatos();
 
     }
 }
